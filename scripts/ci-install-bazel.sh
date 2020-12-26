81ffe4e37b0bfcfc1e2984ae32872f5008763152
sugarraysam <samuel.blaisdowdy@protonmail.com>
2020-12-26 14:14:14 -0600

    fix bazel install in travis CI

--- scripts/ci-install-bazel.sh
+++ scripts/ci-install-bazel.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 TMPDIR="${TMPDIR:-/tmp}"
-BAZEL_DEB="bazel_${BAZEL_VERSION}_amd64.deb"
-BAZEL_URL="https://github.com/bazelbuild/bazel/releases/download/${BAZEL_VERSION}/bazel_${BAZEL_DEB}"
+BAZEL_DEB="bazel_${BAZEL_VERSION}-linux-x86_64.deb"
+BAZEL_URL="https://github.com/bazelbuild/bazel/releases/download/${BAZEL_VERSION}/${BAZEL_DEB}"
 
 curl -sL --output "${TMPDIR}/${BAZEL_DEB}" "${BAZEL_URL}"
 sudo dpkg --force-all -i "${TMPDIR}/${BAZEL_DEB}"
 
-bazel --version >/dev/null 2>&1
+bazel --version
